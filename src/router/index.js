99f0372f82514e65b5703b17e2f27a9ab5c0cff7
ghamza-dev <elgassaihamzam54@gmail.com>
2022-04-05 22:03:05 +0200

    Add navigation guard

--- src/router/index.js
+++ src/router/index.js
@@ -3,12 +3,13 @@ import HomeView from '../views/HomeView.vue'
 import RegisterView from '../views/RegisterView.vue'
 import LoginView from '../views/LoginView.vue'
 import AppointementView from '../views/AppointementView.vue'
+import store from '@/store'
 
 const routes = [
   {
     path: '/',
     name: 'home',
-    component: HomeView
+    component: HomeView,
   },
   {
     path: '/register',
@@ -23,13 +24,36 @@ const routes = [
   {
     path: '/appointement',
     name: 'appointement',
-    component: AppointementView
+    component: AppointementView,
+    meta:{requiresAuth:true}
   }
 ]
 
 const router = createRouter({
   history: createWebHistory(process.env.BASE_URL),
   routes
+})
+
+router.beforeEach((to,from,next)=>{
+  
+  if(!to.meta?.requiresAuth){
+    console.log(to.meta?.requiresAuth);
+    next();return;
+  }
+
+  if (!localStorage.getItem('userxyz') && to.name != 'login') {
+    store.commit('setUser',[]);
+    store.commit('authToggle',false);
+    next({name:'login'});return;
+  }
+
+  if (!store.state.auth || !store.state.user) {
+    let user = JSON.parse(localStorage.getItem('userxyz'));
+    store.commit('setUser',user);
+    store.commit('authToggle',true);
+  }
+
+  next();
 })
 
 export default router
