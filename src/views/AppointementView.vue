f5f82fb9b6aa92594660805a644fc3285731b153
ghamza-dev <elgassaihamzam54@gmail.com>
2022-04-05 12:04:20 +0200

    Center and imporove calender UI

--- src/views/AppointementView.vue
+++ src/views/AppointementView.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="max-w-xl mt-24 rounded-md m-auto h-96 bg-indigo-300 mx-2">
+  <div class="max-w-xl mt-24 rounded-md m-auto h-96 bg-indigo-300 sm:mx-auto mx-2">
     <form>
       <div class="flex flex-col pl-3 pt-3">
         <label class="text-gray-dark" for="date">Select Date</label>
@@ -8,8 +8,8 @@
     </form>
     <hr class="mt-3 text-emerald-100">
     <div class="grid grid-cols-4 grid-rows-4 justify-items-stretch items-stretch gap-4 px-3 pt-3">
-      <div v-for="sc in $store.state.schedule" :key="sc.scheduleId" class="h-24  border text-center pt-2 relative border-emerald-100 rounded cursor-pointer">
-        <p class="text-gray-dark text-lg">{{sc.label}}</p>
+      <div v-for="sc in $store.state.schedule" :key="sc.scheduleId" class="h-24  border text-center pt-2 relative border-emerald-100 rounded cursor-pointer bg-white bg-opacity-30">
+        <p class="text-gray-dark text-lg underline">{{sc.label}}</p>
         <div class="w-6 h-6 bg-primary rounded-full absolute bottom-1 right-1"></div>
       </div>
     </div>
