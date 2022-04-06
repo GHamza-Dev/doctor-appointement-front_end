a612ea28d7b00462f0d0430e43da8652d460e913
ghamza-dev <elgassaihamzam54@gmail.com>
2022-04-07 01:52:25 +0200

    Create footer

---
+++ src/components/Footer.vue
@@ -0,0 +1,21 @@
+<template>
+  <footer class="text-center bg-gray-dark text-white p-4 text-sm relative bottom-0 w-screen">
+      <p>&copy; {{year}} Docchama, All Rights Reserved</p>
+  </footer>
+</template>
+
+<script>
+export default {
+    computed:{
+        year(){
+            let date = new Date();
+            return date.getFullYear();
+        }
+    }
+    // max-w-xl m-auto
+}
+</script>
+
+<style>
+
+</style>
