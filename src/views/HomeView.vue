2811953a4134515eeeacb5bf4bc2b3e5c8570d36
ghamza-dev <elgassaihamzam54@gmail.com>
2022-04-24 02:32:06 +0200

    Add route name

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -6,7 +6,7 @@
         <p class="text-gray-dark  sm:text-3xl text-2xl mt-4">to live for the whole family</p>
         <p class="text-gray-dark text-lg sm:text-xl mt-4">Professional care for your health</p>
         <button class="mt-9">
-          <router-link class="bg-primary hover:bg-secondary text-white p-3 rounded-3xl" to='/'>Make An Appoientement</router-link>
+          <router-link class="bg-primary hover:bg-secondary text-white p-3 rounded-3xl" :to="{name:'appointement'}">Make An Appoientement</router-link>
         </button>
       </div>
     </div>
